43b37781d2fba3f5ebb831c80368876afdf49eb7
Juan De La Torre <juandelatorre@campus-017-251.ucdavis.edu>
2021-11-05 19:41:29 -0700

    adding queries and retirement info csv

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -1,1 +1,26 @@
-tbd
+-- retirement eligibility
+select first_name, last_name
+into retirement_info
+from employees
+where (birth_date between '1952-01-01' and '1955-12-31') 
+and (hire_date between '1985-01-01' AND '1988-12-31');
+
+--SELECT first_name, last_name
+--FROM employees
+--WHERE birth_date BETWEEN '1952-01-01' AND '1954-12-31';
+
+--SELECT first_name, last_name
+--FROM employees
+--WHERE birth_date BETWEEN '1952-01-01' AND '1953-12-31';
+
+--SELECT first_name, last_name
+--FROM employees
+--WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- number of employees retiring
+select count(first_name)
+from employees
+where (birth_date between '1952-01-01' and '1955-12-31') 
+and (hire_date between '1985-01-01' AND '1988-12-31');
+
+select * from retirement_info;
